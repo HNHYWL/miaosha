adb27932ea829bdb64f82f8f470f51acb7fcc02d
wangliang <wqwl51@qq.com>
2019-03-16 21:43:11 +0800

    init

---
+++ src/main/java/com/miaoshaproject/App.java
@@ -0,0 +1,36 @@
+package com.miaoshaproject;
+
+
+import com.miaoshaproject.dao.UserDOMapper;
+import com.miaoshaproject.dataobject.UserDO;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication(scanBasePackages = {"com.miaoshaproject"})
+@RestController
+@MapperScan("com.miaoshaproject.dao")
+public class App {
+
+    @Autowired
+    UserDOMapper userDOMapper;
+
+
+
+    @RequestMapping("/")
+    public String home() {
+        UserDO userDO =  userDOMapper.selectByPrimaryKey(1);
+        if (userDO == null) {
+            return "hello";
+        }
+        return "world";
+    }
+
+    public static void main(String[] args) {
+        System.out.println("hello world");
+        SpringApplication.run(App.class, args);
+    }
+}
